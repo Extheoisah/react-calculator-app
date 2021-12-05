be2a1d8ad506dabae5bd56eca4a6cf5191aaf0a7
Theophilus <extheo2019@gmail.com>
2021-12-05 23:26:42 +0100

    made app responsive

--- src/App.js
+++ src/App.js
@@ -144,44 +144,64 @@ function App() {
   );
 
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="prev-operand">
-          {formatOperand(prevOperand)} {operation}
+    <>
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="prev-operand">
+            {formatOperand(prevOperand)} {operation}
+          </div>
+          <div className="curr-operand">{formatOperand(currOperand)}</div>
         </div>
-        <div className="curr-operand">{formatOperand(currOperand)}</div>
+        <button
+          className="span-two"
+          onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+        >
+          AC
+        </button>
+        <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+          DEL
+        </button>
+        <OperationButton operation="÷" dispatch={dispatch} />
+        <DigitButton digit="1" dispatch={dispatch} />
+        <DigitButton digit="2" dispatch={dispatch} />
+        <DigitButton digit="3" dispatch={dispatch} />
+        <OperationButton operation="*" dispatch={dispatch} />
+        <DigitButton digit="4" dispatch={dispatch} />
+        <DigitButton digit="5" dispatch={dispatch} />
+        <DigitButton digit="6" dispatch={dispatch} />
+        <OperationButton operation="+" dispatch={dispatch} />
+        <DigitButton digit="7" dispatch={dispatch} />
+        <DigitButton digit="8" dispatch={dispatch} />
+        <DigitButton digit="9" dispatch={dispatch} />
+        <OperationButton operation="-" dispatch={dispatch} />
+        <DigitButton digit="." dispatch={dispatch} />
+        <DigitButton digit="0" dispatch={dispatch} />
+        <button
+          className="span-two"
+          onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+        >
+          =
+        </button>
       </div>
-      <button
-        className="span-two"
-        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
-      >
-        AC
-      </button>
-      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
-        DEL
-      </button>
-      <OperationButton operation="÷" dispatch={dispatch} />
-      <DigitButton digit="1" dispatch={dispatch} />
-      <DigitButton digit="2" dispatch={dispatch} />
-      <DigitButton digit="3" dispatch={dispatch} />
-      <OperationButton operation="*" dispatch={dispatch} />
-      <DigitButton digit="4" dispatch={dispatch} />
-      <DigitButton digit="5" dispatch={dispatch} />
-      <DigitButton digit="6" dispatch={dispatch} />
-      <OperationButton operation="+" dispatch={dispatch} />
-      <DigitButton digit="7" dispatch={dispatch} />
-      <DigitButton digit="8" dispatch={dispatch} />
-      <DigitButton digit="9" dispatch={dispatch} />
-      <OperationButton operation="-" dispatch={dispatch} />
-      <DigitButton digit="." dispatch={dispatch} />
-      <DigitButton digit="0" dispatch={dispatch} />
-      <button
-        className="span-two"
-        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
-      >
-        =
-      </button>
-    </div>
+      <div className="footer">
+        <pre>
+          <a
+            href="https://github.com/Extheoisah/react-calculator-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            source-code
+          </a>
+          <a
+            href="https://extheo.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Portfolio website
+          </a>
+        </pre>
+      </div>
+    </>
   );
 }
 
